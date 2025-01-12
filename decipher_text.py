503793c2153edf87b0867a8a2ded103821337508
Nikhil Gupta <177606878+BitByNIK@users.noreply.github.com>
2025-01-12 22:21:24 +0530

    logic implemented but not working

--- decipher_text.py
+++ decipher_text.py
@@ -1,62 +1,115 @@
 from collections import Counter
 import nltk
-from nltk import bigrams
+from nltk import ngrams
+import math
+import random
 import ssl
-import re
 
 ssl._create_default_https_context = ssl._create_unverified_context
 nltk.download("gutenberg", quiet=True)
 
 SF = {}
-SDF = {}
-DF = {}
-DDF = {}
+QF = {}
+
+SR = {}
+CR = {}
+
+ignore_chars = {' ', ',', ';', '!', '.'}
 
 
-def frequency_analysis(standard, text=""):
+def swap_mappings(d, pd):
+    if len(d) < 2:
+        print("Dictionary must have at least two keys to swap.")
+        return d
+
+    key1 = random.choices(list(pd.keys()), weights=pd.values())[0]
+    key2 = random.choice(list(d.keys()))
+
+    d[key1], d[key2] = d[key2], d[key1]
+
+    return d
+
+
+def frequency_analysis():
     global SF
-    global SDF
-    global DF
-    global DDF
+    global QF
 
-    if standard:
-        from nltk.corpus import gutenberg
-        text = gutenberg.raw()
+    from nltk.corpus import gutenberg
+    text = gutenberg.raw()
+    filtered_text = ''.join([char.lower()
+                            for char in text if 'a' <= char <= 'z'])
 
-    letters = [char.lower() for char in text if char.isalpha()]
-    total_letters = len(letters)
+    quadgrams = ngrams(filtered_text, 4)
 
-    bigram_list = list(bigrams(letters))
+    letter_count = Counter(filtered_text)
+    quadgram_count = Counter(quadgrams)
 
-    letter_count = Counter(letters)
-    bigram_count = Counter(bigram_list)
-    total_bigrams = sum(bigram_count.values())
+    total_letters = sum(letter_count.values())
+    total_quadgrams = sum(quadgram_count.values())
 
-    temp1 = {letter: (count / total_letters) * 100 for letter,
-             count in letter_count.items()}
-    temp2 = {bigram: (count / total_bigrams) * 100 for bigram,
-             count in bigram_count.items()}
-
-    if standard:
-        SF, SDF = temp1, temp2
-    else:
-        DF, DDF = temp1, temp2
+    SF = {letter: (count / total_letters) for letter,
+          count in letter_count.items()}
+    QF = {quadgram: freq / total_quadgrams for quadgram,
+          freq in quadgram_count.items()}
 
 
-def getFitness():
-    letter_diff = sum(abs(SF[chr(i + 97)] - DF.get(chr(i + 97), 0.0))
-                      for i in range(26))
-    bigram_diff = sum(abs(SDF.get((chr(i + 97), chr(j + 97)), 0.0) - DDF.get((chr(i + 97), chr(j + 97)), 0.0))
-                      for i in range(26) for j in range(26))
+def pd_preprocessing(ciphertext):
+    global SR
+    global CR
 
-    return (1 - (letter_diff + bigram_diff) / 4) ** 8
+    text = ciphertext.lower()
+    ciphertext_letter_count = Counter(
+        char for char in text if char not in ignore_chars)
+    total_ciphertext_letters = sum(ciphertext_letter_count.values())
+    CF = {letter: (count / total_ciphertext_letters)
+          for letter, count in ciphertext_letter_count.items()}
+
+    dummy_index = 1
+    while len(CF) < 26:
+        CF[f"_dummy{dummy_index}"] = 0.0
+        dummy_index += 1
+
+    sorted_SF = sorted(SF.keys(), key=lambda x: SF[x])
+    sorted_CF = sorted(CF.keys(), key=lambda x: CF[x])
+
+    SR = {letter: rank for rank, letter in enumerate(sorted_SF, start=1)}
+    CR = {letter: rank for rank, letter in enumerate(sorted_CF, start=1)}
+
+
+def generate_probabilty_distribution(current_key):
+    heuristic_values = {}
+    for letter in CR:
+        decrypted_letter = current_key[letter]
+        heuristic_values[letter] = max(
+            1, abs(CR[letter] - SR[decrypted_letter]))
+
+    total_heuristics = sum(heuristic_values.values())
+    if total_heuristics == 0:
+        return {k: 1 / len(heuristic_values) for k in heuristic_values}
+    return {k: v / total_heuristics for k, v in heuristic_values.items()}
+
+
+def getFitness(text):
+    quadgrams = ngrams(text, 4)
+    fitness = 0
+
+    for quadgram in quadgrams:
+        if quadgram in QF:
+            fitness += math.log10(QF[quadgram])
+
+    return fitness
+
+
+def get_decrypted_text(ciphertext, current_key):
+    return ''.join(current_key[char.lower()].upper() if char.isupper() else current_key[char.lower()]
+                   if char.lower() in current_key else char
+                   for char in ciphertext)
 
 
 def substitution_decipher(ciphertext):
-    frequency_analysis(True)
+    frequency_analysis()
 
-    ignore_chars = {' ', ',', ';', '!', '.'}
-    unique_cipher_chars = list(set(ciphertext) - ignore_chars)
+    unique_cipher_chars = list(set(ciphertext.lower()) - ignore_chars)
     lowercase_alphabets = [chr(i) for i in range(ord('a'), ord('z') + 1)]
 
     dummy_index = 1
@@ -64,17 +117,43 @@ def substitution_decipher(ciphertext):
         unique_cipher_chars.append(f"_dummy{dummy_index}")
         dummy_index += 1
 
-    cipher_mapping = {
-        unique_cipher_chars[i]: lowercase_alphabets[i] for i in range(26)}
+    random.shuffle(lowercase_alphabets)
+    best_key = {key: value for key, value in zip(
+        unique_cipher_chars, lowercase_alphabets)}
+    best_key_fitness = getFitness(get_decrypted_text(ciphertext, best_key))
 
-    filtered_cipher_mapping = {
-        k: v for k, v in cipher_mapping.items() if not re.match(r'^_dummy\d+$', k)}
-    translation_table = str.maketrans(filtered_cipher_mapping)
+    pd_preprocessing(ciphertext.lower())
 
-    frequency_analysis(False, ciphertext.translate(translation_table))
-    print(filtered_cipher_mapping, DF['e'], DDF.get(('t', 'h'), 0.0))
-    print(getFitness())
+    for _ in range(100):
+        checks = 0
+
+        random.shuffle(lowercase_alphabets)
+        best_iteration_key = {key: value for key, value in zip(
+            unique_cipher_chars, lowercase_alphabets)}
+        best_iteration_key_fitness = getFitness(
+            get_decrypted_text(ciphertext, best_iteration_key))
+
+        pd = generate_probabilty_distribution(best_iteration_key)
+
+        while True:
+            new_key = swap_mappings(best_iteration_key, pd)
+            new_key_fitness = getFitness(
+                get_decrypted_text(ciphertext, new_key))
+
+            if new_key_fitness > best_iteration_key_fitness:
+                best_iteration_key, best_iteration_key_fitness = new_key, new_key_fitness
+                pd = generate_probabilty_distribution(new_key)
+                checks = 0
+
+            checks = checks + 1
+            if checks >= 600:
+                break
+
+        if best_iteration_key_fitness > best_key_fitness:
+            best_key, best_key_fitness = best_iteration_key, best_iteration_key_fitness
+
+    print(get_decrypted_text(ciphertext, best_key))
 
 
 if __name__ == "__main__":
-    substitution_decipher("1981y, $pp1n1yuux oq@ 2@3s5u1n $p 1981y, 1v y n$s9o2x 19 v$soq yv1y. 1o 1v oq@ v@6@9oq uy27@vo n$s9o2x 5x y2@y, oq@ v@n$98 0$vo 3$3su$sv n$s9o2x, y98 oq@ 0$vo 3$3su$sv 8@0$n2ynx 19 oq@ #$2u8. 5$s98@8 5x oq@ 1981y9 $n@y9 $9 oq@ v$soq, oq@ y2y51y9 v@y $9 oq@ v$soq#@vo, y98 oq@ 5yx $p 5@97yu $9 oq@ v$soq@yvo, 1o vqy2@v uy98 5$28@2v #1oq 3yw1voy9 o$ oq@ #@vo; nq19y, 9@3yu, y98 5qsoy9 o$ oq@ 9$2oq; y98 5y97uy8@vq y98 0xy90y2 o$ oq@ @yvo. 19 oq@ 1981y9 $n@y9, 1981y 1v 19 oq@ 61n191ox $p v21 uy9wy y98 oq@ 0yu816@v; 1ov y98y0y9 y98 91n$5y2 1vuy98v vqy2@ y 0y21o10@ 5$28@2 #1oq oqy1uy98, 0xy90y2 y98 198$9@v1y. 7$$8, 9$# os29 p$2 oq@ v@n$98 3y2o $p oq@ 4s@vo1$9, 7$$8 usnw!")
+    substitution_decipher(input())
