9234166ac964b7dfae0d16a007540b4822b0c9c7
Nikhil Gupta <177606878+BitByNIK@users.noreply.github.com>
2025-01-12 23:28:40 +0530

    enhancements

--- decipher_text.py
+++ decipher_text.py
@@ -1,12 +1,6 @@
 from collections import Counter
-import nltk
-from nltk import ngrams
 import math
 import random
-import ssl
-
-ssl._create_default_https_context = ssl._create_unverified_context
-nltk.download("gutenberg", quiet=True)
 
 SF = {}
 QF = {}
@@ -37,23 +31,23 @@ def frequency_analysis():
     global SF
     global QF
 
-    from nltk.corpus import gutenberg
-    text = gutenberg.raw()
-    filtered_text = ''.join([char.lower()
-                            for char in text if 'a' <= char <= 'z'])
+    with open("big.txt", "r", encoding="utf-8") as file:
+        text = file.read()
+        filtered_text = ''.join([char.lower()
+                                 for char in text if 'a' <= char <= 'z'])
 
-    quadgrams = ngrams(filtered_text, 4)
+        quadgrams = zip(*[filtered_text[i:] for i in range(4)])
 
-    letter_count = Counter(filtered_text)
-    quadgram_count = Counter(quadgrams)
+        letter_count = Counter(filtered_text)
+        quadgram_count = Counter(quadgrams)
 
-    total_letters = sum(letter_count.values())
-    total_quadgrams = sum(quadgram_count.values())
+        total_letters = sum(letter_count.values())
+        total_quadgrams = sum(quadgram_count.values())
 
-    SF = {letter: (count / total_letters) for letter,
-          count in letter_count.items()}
-    QF = {quadgram: freq / total_quadgrams for quadgram,
-          freq in quadgram_count.items()}
+        SF = {letter: (count / total_letters) for letter,
+              count in letter_count.items()}
+        QF = {quadgram: freq / total_quadgrams for quadgram,
+              freq in quadgram_count.items()}
 
 
 def pd_preprocessing(ciphertext):
@@ -88,7 +82,7 @@ def generate_probabilty_distribution(current_key):
 
 
 def getFitness(text):
-    quadgrams = ngrams(text, 4)
+    quadgrams = zip(*[text[i:] for i in range(4)])
     fitness = 0
 
     for quadgram in quadgrams:
@@ -104,24 +98,28 @@ def get_decrypted_text(ciphertext, current_key):
                    for char in ciphertext)
 
 
+def get_key_and_fitness(ciphertext):
+    random.shuffle(lowercase_alphabets)
+
+    key = {key: value for key, value in zip(
+        ciphertext_chars, lowercase_alphabets)}
+    fitness = getFitness(get_decrypted_text(ciphertext, key))
+
+    return key, fitness
+
+
 def substitution_decipher(ciphertext):
     frequency_analysis()
 
-    random.shuffle(lowercase_alphabets)
-    best_key = {key: value for key, value in zip(
-        ciphertext_chars, lowercase_alphabets)}
-    best_key_fitness = getFitness(get_decrypted_text(ciphertext, best_key))
+    best_key, best_key_fitness = get_key_and_fitness(ciphertext)
 
     pd_preprocessing(ciphertext.lower())
 
     for _ in range(100):
         checks = 0
 
-        random.shuffle(lowercase_alphabets)
-        best_iteration_key = {key: value for key, value in zip(
-            ciphertext_chars, lowercase_alphabets)}
-        best_iteration_key_fitness = getFitness(
-            get_decrypted_text(ciphertext, best_iteration_key))
+        best_iteration_key, best_iteration_key_fitness = get_key_and_fitness(
+            ciphertext)
 
         pd = generate_probabilty_distribution(best_iteration_key)
 
@@ -143,9 +141,9 @@ def substitution_decipher(ciphertext):
             best_key, best_key_fitness = best_iteration_key, best_iteration_key_fitness
 
     sorted_key = sorted(best_key.items(), key=lambda item: item[1])
-    print("".join(k for k, v in sorted_key))
-    print(get_decrypted_text(ciphertext, best_key))
+    print("Key: ", "".join(k for k, v in sorted_key))
+    print("Decrypted Text: ", get_decrypted_text(ciphertext, best_key))
 
 
 if __name__ == "__main__":
-    substitution_decipher(input())
+    substitution_decipher(input("Enter Ciphertext: "))
